e4837c818e6cf28d4756b52cd4ba98a4eedc7ff5
amarinos10 <alexmarinos967@gmail.com>
2025-09-22 09:34:54 +0100

    Created venv enviroment for project

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,7 +1,6 @@
 # import modules
 import os
 import pdfplumber
-import gTTs
 
 # assign directories
 directory_invoices = 'SRC_INVOICE_FILE_PATH'
@@ -13,9 +12,8 @@ invoices_file_name = os.path.basename(directory_invoices)
 # iterate over files in src folder
 for invoices_pdf in os.listdir(directory_invoices):
     #  using pdfplumber to extract text from pdf invoices files
-    with pdfplumber.open("*.pdf") as pdf, open(invoices_file_name + ".txt", "w",encoding="utf-8") as f:
+    with pdfplumber.open("*.pdf") as pdf, open(  invoices_file_name + ".txt", "w",encoding="utf-8") as f:
         for page in pdf.pages:
             t = page.extract_text()
             if t:
                 f.write(t + '\n')
-                f.save(directory_invoices_txt)
